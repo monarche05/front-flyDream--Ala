579739228ddef22e273d0d71ebff3c63cf085176
Monarche <petiot.jonathan@gmail.com>
2022-04-22 15:57:52 +0200

    v1.1 correctif (add Com)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,9 +24,21 @@ export class AppComponent {
         alert(data);
       });
   }
+
+  //getHelloWorld2() est une fonction de teste
+  // associé à un deuxiéme bouton  hors live pour
+  // trouver l'erreurs:
+  //   Access to XMLHttpRequest at
+  //  'http://localhost:3000/' from origin 'http://localhost:4200'
+  //   has been blocked by CORS policy:
+  //   Response to preflight request doesn't pass access
+  //   control check: No 'Access-Control-Allow-Origin'
+  //   header is present on the requested resource.
+
   getHelloWorld2() {
     const headers = {
       headers: new HttpHeaders({
+        'Acces-Controle-Allow-Headers': 'Accepte, Content-Type',
         'Access-Control-Allow-Origin': 'http://localhost:3000',
         'Access-Control-Allow-Methodes': 'GET ,POST , PUT, DELETE, OPTIONS',
       }),
